4e9ad2dd811aca2b5c88d8a0441d5791eee1d952
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-10 17:54:14 +0900

    feat: 메인 화면의 헤더바를 구현함

---
+++ app/src/main/java/com/example/composecarrot/ui/component/HeaderBar.kt
@@ -0,0 +1,56 @@
+package com.example.composecarrot.ui.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composecarrot.R
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderBar() {
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("가락2동", fontWeight = FontWeight.W800, fontSize = 24.sp)
+            Icon(
+                painter = painterResource(R.drawable.ic_direction_down),
+                contentDescription = "드롭다운 보여주는 아이콘"
+            )
+        }
+        Row (
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_search),
+                contentDescription = "검색 아이콘",
+                tint = Color(0xFF212123)
+            )
+            Icon(
+                painter = painterResource(R.drawable.ic_menu_hamburger),
+                contentDescription = "더보기 아이콘",
+                tint = Color(0xFF212123)
+            )
+            Icon(
+                painter = painterResource(R.drawable.ic_notification_lg),
+                contentDescription = "알림 아이콘",
+                tint = Color(0xFF212123)
+            )
+        }
+    }
+}
