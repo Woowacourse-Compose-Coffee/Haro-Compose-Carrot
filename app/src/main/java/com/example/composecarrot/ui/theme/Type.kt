f95015a154b68978bdced89a71ff1e91f8c1f326
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-03-10 23:42:33 +0900

    style: Pretendard 폰트를 추가함

--- app/src/main/java/com/example/composecarrot/ui/theme/Type.kt
+++ app/src/main/java/com/example/composecarrot/ui/theme/Type.kt
@@ -2,9 +2,24 @@ package com.example.composecarrot.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.composecarrot.R
+
+val pretendardFamily = FontFamily(
+    Font(R.font.pretendard_thin, FontWeight.W100),
+    Font(R.font.pretendard_extralight, FontWeight.W200),
+    Font(R.font.pretendard_light, FontWeight.W300),
+    Font(R.font.pretendard_regular, FontWeight.W400),
+    Font(R.font.pretendard_medium, FontWeight.W500),
+    Font(R.font.pretendard_semibold, FontWeight.W600),
+    Font(R.font.pretendard_bold, FontWeight.W700),
+    Font(R.font.pretendard_extrabold, FontWeight.W800),
+    Font(R.font.pretendard_black, FontWeight.W900),
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
